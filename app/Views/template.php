3538c40470aa28961c32a32b4fb19b29d13c1637
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-01-11 17:12:33 +0700

    Edit Landing page

--- app/Views/template.php
+++ app/Views/template.php
@@ -10,28 +10,36 @@
 </head>
 
 <body>
-    <img class="bg" src="/images/background.svg" alt="">
-    <div class="container">
-        <nav class="navbar navbar-expand-lg navbar-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#"><img src="/images/logo.svg" alt=""></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav me-auto">
-                        <li class="nav-item">
-                            <a class="nav-link text-dark" href="#">Adoption</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-dark" href="#">Breeding</a>
-                        </li>
-                    </ul>
-                    <a href="#" class="btn text-dark me-2">Sign in</a>
-                    <a href="#" class="btn bg-warning">Sign up</a>
+    <div class="container-fluid bg-light">
+        <div class="container bg-light">
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#"><img src="/images/logo.svg" alt=""></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav me-auto">
+                            <li class="nav-item">
+                                <a class="nav-link text-dark" href="#">Adoption</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-dark" href="#">Breeding</a>
+                            </li>
+                        </ul>
+                        <a href="#" class="link-dark me-3">Sign in</a>
+                        <a href="#" class="btn btn-warning">Sign up</a>
+                    </div>
+                </div>
+            </nav>
+            <div class="hero-image">
+                <div class="p-5 rounded-lg text-light text-center">
+                    <h1 class="display-4">Find your new friends</h1>
+                    <p class="lead">Inguanku is a platform that connects donators and adopters of pets.</p>
+                    <a class="btn btn-outline-warning btn-lg" href="#" role="button">Find here!</a>
                 </div>
             </div>
-        </nav>
+        </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
