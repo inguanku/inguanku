dedc392147ae06c710e58758aa99aacbcef1d6ca
chevi candra <chevicandra2@gmail.com>
2021-01-12 17:30:13 +0700

    adjusting navbar background; changed hero-image style , link text color & rounded button

--- app/Views/template.php
+++ app/Views/template.php
@@ -10,10 +10,10 @@
 </head>
 
 <body>
-    <div class="container-fluid bg-light">
-        <div class="container bg-light">
-            <nav class="navbar navbar-expand-lg bg-light">
-                <div class="container-fluid">
+    <div class="container-fluid px-0 bg-light hero-image">
+        <div class="container-fluid bg-transparent">
+            <nav class="navbar navbar-expand-lg">
+                <div class="container">
                     <a class="navbar-brand" href="#"><img src="/images/logo.svg" alt=""></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
@@ -21,23 +21,24 @@
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav me-auto">
                             <li class="nav-item">
-                                <a class="nav-link text-dark" href="#">Adoption</a>
+                                <a class="nav-link text-light" href="#">Adoption</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link text-dark" href="#">Breeding</a>
+                                <a class="nav-link text-light" href="#">Breeding</a>
                             </li>
                         </ul>
-                        <a href="#" class="link-dark me-3">Sign in</a>
-                        <a href="#" class="btn btn-warning">Sign up</a>
+                        <a href="#" class="link-light me-3">Sign in</a>
+                        <a href="#" class="btn btn-warning rounded-pill">Sign up</a>
                     </div>
                 </div>
             </nav>
-            <div class="hero-image">
-                <div class="p-5 rounded-lg text-light text-center">
-                    <h1 class="display-4">Find your new friends</h1>
-                    <p class="lead">Inguanku is a platform that connects donators and adopters of pets.</p>
-                    <a class="btn btn-outline-warning btn-lg" href="#" role="button">Find here!</a>
-                </div>
+        </div>
+        <div class="container">
+
+            <div class="p-5 rounded-lg text-light text-center">
+                <h1 class="display-4">Find your new friends</h1>
+                <p class="lead">Inguanku is a platform that connects donators and adopters of pets.</p>
+                <a class="btn btn-outline-warning btn-lg" href="#" role="button">Find here!</a>
             </div>
         </div>
     </div>
