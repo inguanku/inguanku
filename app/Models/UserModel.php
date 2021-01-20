09abd698877057c4c83d3bf38359ca540831cbd2
ilhammfadilah <ilhamfadilah92@gmail.com>
2021-01-20 21:40:07 +0700

    add Model for UserModel.

---
+++ app/Models/UserModel.php
@@ -0,0 +1,9 @@
+<?php 
+namespace App\Models;
+use CodeIgniter\Model;
+
+class UserModel extends Model {
+    protected $table = 'tbl_user';
+    protected $allowedFields = ['name', 'email', 'passwd', 'phone', 'address'];
+}
+?>
