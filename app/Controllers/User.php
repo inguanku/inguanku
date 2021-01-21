07560d1e490afc202972db5995cec8f0a823fbb1
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-01-21 13:40:00 +0700

    add login and change address to city

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -18,7 +18,7 @@ class User extends BaseController
         $rules = [
             'name'      => 'required|min_length[3]|max_length[50]',
             'email'     => 'required|min_length[6]|max_length[50]|valid_email|is_unique[tbl_user.email]',
-            'passwd'    => 'required|min_length[3]|max_length[40]',
+            'passwd'    => 'required|min_length[3]|max_length[100]',
             'passConf'  => 'matches[passwd]',
             'phone'     => 'required',
             'city'      => 'required'
@@ -29,16 +29,52 @@ class User extends BaseController
             $data = [
                 'name' => $this->request->getVar('name'),
                 'email' => $this->request->getVar('email'),
-                'passwd' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT),
+                'passwd' => password_hash($this->request->getVar('passwd'), PASSWORD_DEFAULT),
                 'phone' => $this->request->getVar('phone'),
                 'city' => $this->request->getVar('city')
             ];
 
-            $model->insert($data);
+            $model->save($data);
             return redirect()->to('./login');
         } else {
             $data['validation'] = $this->validator;
             echo view('user/register', $data);
         }
+    }
+
+    public function auth()
+    {
+        $session = session();
+        $model = new UserModel();
+        $email = $this->request->getVar('email');
+        $password = $this->request->getVar('password');
+        $data = $model->where('email', $email)->first();
+        if ($data) {
+            $pass = $data['passwd'];
+            $verify_pass = password_verify($password, $pass);
+            if ($verify_pass) {
+                $ses_data = [
+                    'name'       => $data['name'],
+                    'email'     => $data['email'],
+                    'city'    => $data['city'],
+                    'logged_in'     => TRUE
+                ];
+                $session->set($ses_data);
+                return redirect()->to('/home');
+            } else {
+                $session->setFlashdata('msg', 'Wrong Password');
+                return redirect()->to('/login');
+            }
+        } else {
+            $session->setFlashdata('msg', 'Email not Found');
+            return redirect()->to('/login');
+        }
+    }
+
+    public function logout()
+    {
+        $session = session();
+        $session->destroy();
+        return redirect()->to('/home');
     }
 }
