947e84c249e6bc6052d8aef8e738e1eab5621b71
Dwi Fitriyanto <dwifitriyanto123@gmail.com>
2021-02-04 07:56:54 +0700

    tidy up

--- app/Models/RequestModel.php
+++ app/Models/RequestModel.php
@@ -15,9 +15,6 @@ class RequestModel extends Model
     {
         $userId = $data['user_id'];
         $postId = $data['post_id'];
-        // return $this->db->table('tbl_request')
-        //                 ->where('user_id', $userId)
-        //                 ->where('post_id', $postId)->getRow();
         $db = \Config\Database::connect();
         $query = $db->query("SELECT request_id FROM tbl_request WHERE user_id = $userId AND post_id = $postId");
         return $row = $query->getRow();
